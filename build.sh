7dd5d30914ed1482f5a1b7f2a9928ff440d4a675
Tom Atwood <tom@tmatwood.com>
2024-12-29 14:12:09 -0500

    feature/#1: Added more tests and all base packages (brew, gobrew, nvm).

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ SHORTSHA=$(gitversion /showvariable ShortSha)
 
 IMAGE="tmatwood/gitlab-runner-base"
 
-echo "Building version $VERSION-$SHORTSHA."
+echo "Building version ${VERSION}-${SHORTSHA}."
 
-docker buildx build -t $IMAGE:$VERSION-$SHORTSHA .
-docker tag $IMAGE:$VERSION-$SHORTSHA $IMAGE:latest
+docker buildx build -t "${IMAGE}":"${VERSION}"-"${SHORTSHA}" .
+docker tag "${IMAGE}":"${VERSION}"-"${SHORTSHA}" "${IMAGE}":latest
 
-docker push $IMAGE:$VERSION-$SHORTSHA
-docker push $IMAGE:latest
+docker push "${IMAGE}":"${VERSION}"-"${SHORTSHA}"
+docker push "${IMAGE}":latest
